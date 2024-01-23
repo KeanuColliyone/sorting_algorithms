7ceafdf974b153fea73e48975ca1a0e415c941c1
Kenan Kotzee <frostcolliyone@gmail.com>
2024-01-23 14:47:28 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
+ * bubble_sort - Sorts array of integers in ascending order using Bubble sort
  * @array: Pointer to the array to be sorted
  * @size: Size of the array
  */

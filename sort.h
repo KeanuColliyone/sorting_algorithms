45240ff3d3d5b8f0c89b01b8b6d0c7776990518b
Kenan Kotzee <frostcolliyone@gmail.com>
2024-01-23 14:51:43 -0800

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -3,11 +3,17 @@
 
 #include <stddef.h>
 
-/* Function prototype for bubble_sort */
-void bubble_sort(int *array, size_t size);
+/* Structure for doubly linked list */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
-/* Function prototype for print_array */
+/* Function prototypes for print_array and print_list */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
 
